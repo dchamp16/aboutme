923ed37f62fa44552f4aa3c3bdfea7e3d5391a9e
Peter Ramos <dchamp16@gmail.com>
2022-10-25 20:40:43 -0600

    added localtime in header

---
+++ components/Clock.js
@@ -0,0 +1,13 @@
+const Clock = () => {
+  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>{time}</div>;
+};
